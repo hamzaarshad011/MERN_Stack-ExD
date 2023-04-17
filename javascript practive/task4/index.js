d08c0ebf4a885d213dec24ed5ec6a83d1c4054a4
Hamza Arshad <129472341+hamzaarshad011@users.noreply.github.com>
2023-04-17 12:23:58 +0500

    Add files via upload

---
+++ javascript practive/task4/index.js
@@ -0,0 +1,8 @@
+function changeObjectValue(obj, key, value) {
+
+    obj[key] = value;
+    return obj;
+  }
+  const person = { name: 'Jane', age: 30 };
+  changeObjectValue(person, 'age', 35);
+  console.log(person); 
