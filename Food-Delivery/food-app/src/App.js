15a9541c3dd9f21c6b1c798dae01944bbe58ed7c
DESKTOP-584N16V\Hamza <hamzaarshad.29feb@gmail.com>
2023-05-25 12:37:12 +0500

    React router dom setup

---
+++ Food-Delivery/food-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./screens/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
